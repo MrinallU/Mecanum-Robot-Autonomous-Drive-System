d748193cd3204eae7ed869fc079650c41bfb59a7
Mrinall Umasudhan <bossguy124567@gmail.com>
2022-09-06 22:25:28 -0400

    Update Odometry.java

--- Mecanum_2/Modules/Odometry.java
+++ Mecanum_2/Modules/Odometry.java
@@ -3,112 +3,115 @@ package org.firstinspires.ftc.teamcode.Mecanum_2.Modules;
 import org.firstinspires.ftc.teamcode.Utils.Angle;
 import org.firstinspires.ftc.teamcode.Utils.Point;
 
+import virtual_robot.util.AngleUtils;
+
 public class Odometry {
-  // Constants
-  public final double WHEEL_DIAMETER = 4;
-  public final double INTER_WHEEL_WIDTH = 16;
-  public final double INTER_WHEEL_LENGTH = 14;
-  public final double TICKS_PER_DRIVE_ROTATION = 1120;
-  public final double TICKS_PER_ENCODER_ROTATION = 1120;
-  public final double ENCODER_WHEEL_DIAMETER = 2;
-  private final double ENCODER_TICKS_PER_REVOLUTION = 1120;
-  private final double ENCODER_WHEEL_CIRCUMFERENCE = Math.PI * 2.0 * (ENCODER_WHEEL_DIAMETER * 0.5);
-  private final double ENCODER_WIDTH = 12.0;
+    // Constants
+    public final double WHEEL_DIAMETER = 4;
+    public final double INTER_WHEEL_WIDTH = 16;
+    public final double INTER_WHEEL_LENGTH = 14;
+    public final double TICKS_PER_DRIVE_ROTATION = 1120;
+    public final double TICKS_PER_ENCODER_ROTATION = 1120;
+    public final double ENCODER_WHEEL_DIAMETER = 2;
+    private final double ENCODER_TICKS_PER_REVOLUTION = 1120;
+    private final double ENCODER_WHEEL_CIRCUMFERENCE = Math.PI * 2.0 * (ENCODER_WHEEL_DIAMETER*0.5);
+    private final double ENCODER_WIDTH = 12.0;
 
-  private boolean verbose = false;
-  public String outStr = "";
+    private boolean verbose = false;
+    public String outStr = "";
 
-  // Variables
-  private double xPos, yPos, angle;
-  private double lastLeftEnc, lastRightEnc, lastNormalEnc;
+    // Variables
+    private double xPos, yPos, angle;
+    private double lastLeftEnc, lastRightEnc, lastNormalEnc;
 
-  public Odometry(double xPos, double yPos, double angle) {
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.angle = angle;
-  }
-
-  public Odometry(double angle) {
-    this.xPos = 0;
-    this.yPos = 0;
-    this.angle = angle;
-  }
-
-  public Odometry() {
-    this(0);
-  }
-
-  // https://github.com/Beta8397/virtual_robot/blob/master/TeamCode/src/org/firstinspires/ftc/teamcode/EncBot.java
-  public void updatePosition(double l, double r, double n, double ang) {
-    double dR = r - lastRightEnc;
-    double dL = l - lastLeftEnc;
-    double dN = n - lastNormalEnc;
-
-    double rightDist = dR * ENCODER_WHEEL_CIRCUMFERENCE / ENCODER_TICKS_PER_REVOLUTION;
-    double leftDist = -dL * ENCODER_WHEEL_CIRCUMFERENCE / ENCODER_TICKS_PER_REVOLUTION;
-    double dyR = 0.5 * (rightDist + leftDist);
-    double headingChangeRadians = (rightDist - leftDist) / ENCODER_WIDTH;
-    double dxR = -dN * ENCODER_WHEEL_CIRCUMFERENCE / ENCODER_TICKS_PER_REVOLUTION;
-    double avgHeadingRadians = Math.toRadians(angle) + headingChangeRadians / 2.0;
-    double cos = Math.cos(avgHeadingRadians);
-    double sin = Math.sin(avgHeadingRadians);
-
-    xPos += dxR * sin + dyR * cos;
-    yPos += -dxR * cos + dyR * sin;
-    angle = Angle.normalize(ang);
-    lastNormalEnc = n;
-    lastLeftEnc = l;
-    lastRightEnc = r;
-  }
-
-  public double normalizeAngle(double rawAngle) {
-    double scaledAngle = rawAngle % 360;
-    if (scaledAngle < 0) {
-      scaledAngle += 360;
+    public Odometry(double xPos, double yPos, double angle) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.angle = angle;
     }
 
-    if (scaledAngle > 180) {
-      scaledAngle -= 360;
+    public Odometry(double angle) {
+        this.xPos = 0;
+        this.yPos = 0;
+        this.angle = angle;
     }
 
-    return scaledAngle;
-  }
+    public Odometry() {
+        this(0);
+    }
 
-  public String displayPositions() {
-    return outStr;
-  }
+    //https://github.com/Beta8397/virtual_robot/blob/master/TeamCode/src/org/firstinspires/ftc/teamcode/EncBot.java
+    public void updatePosition(double l, double r, double n, double ang) {
+        double dR = r - lastRightEnc;
+        double dL = l - lastLeftEnc;
+        double dN = n - lastNormalEnc;
 
-  public double getAngle() {
-    return angle;
-  }
+        double rightDist = dR * ENCODER_WHEEL_CIRCUMFERENCE / ENCODER_TICKS_PER_REVOLUTION;
+        double leftDist = -dL * ENCODER_WHEEL_CIRCUMFERENCE / ENCODER_TICKS_PER_REVOLUTION;
+        double dyR = 0.5 * (rightDist + leftDist);
+        double headingChangeRadians = (rightDist - leftDist) / ENCODER_WIDTH;
+        double dxR = -dN * ENCODER_WHEEL_CIRCUMFERENCE / ENCODER_TICKS_PER_REVOLUTION;
+        double avgHeadingRadians = Math.toRadians(angle) + headingChangeRadians / 2.0;
+        double cos = Math.cos(avgHeadingRadians);
+        double sin = Math.sin(avgHeadingRadians);
 
-  public double getX() {
-    return xPos;
-  }
+        xPos += dxR*sin + dyR*cos;
+        yPos += -dxR*cos + dyR*sin;
+        angle = Angle.normalize(ang);
+        lastNormalEnc = n;
+        lastLeftEnc = l;
+        lastRightEnc = r;
+    }
 
-  public double getY() {
-    return yPos;
-  }
+    public double normalizeAngle(double rawAngle) {
+        double scaledAngle = rawAngle % 360;
+        if ( scaledAngle < 0 ) {
+            scaledAngle += 360;
+        }
 
-  public void resetOdometry() {
-    resetOdometry(0, 0, 0);
-  }
+        if ( scaledAngle > 180 ) {
+            scaledAngle -= 360;
+        }
 
-  public void resetOdometry(Point p) {
-    resetOdometry(p.xP, p.yP, p.ang);
-  }
+        return scaledAngle;
+    }
 
-  public void resetOdometry(double xPos, double yPos, double angle) {
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.angle = angle;
-  }
+    public String displayPositions() {
+        return outStr;
+    }
 
-  public void setAngle(double angle) {
-    this.angle = angle;
-  }
+    public double getAngle() {
+        return angle;
+    }
 
-  private String format(double num) {
-    return String.format("%.3f", num);
-  }
+    public double getX() {
+        return xPos;
+    }
+
+    public double getY() {return yPos;}
+
+
+    public void resetOdometry(){
+        resetOdometry(0, 0, 0);
+    }
+
+    public void resetOdometry(Point p){
+        resetOdometry(p.xP, p.yP, p.ang);
+    }
+
+    public void resetOdometry(double xPos, double yPos, double angle){
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.angle = angle;
+    }
+
+
+    public void setAngle(double angle){
+        this.angle = angle;
+    }
+
+    private String format(double num){
+        return String.format("%.3f", num);
+    }
+
 }
